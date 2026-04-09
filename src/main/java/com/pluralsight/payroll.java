15e90128707e8f77820d7b62337d4153b2f8087c
Gabe <Carrollgbusiness@gmail.com>
2026-04-09 17:19:40 -0500

    First Commit

---
+++ src/main/java/com/pluralsight/payroll.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class payroll {
+    public static void main(String[] args) {
+        //add input function
+        /*
+        variables to let them add
+        their name
+        hours worked (double)
+        pay rate (double)
+      */
+        //add if statement to pay 1.5x pay after 40hrs worked
+    }
+}
